c6d593db8df2b941cdba899cf4400a327d20dec7
NazirahShahmina <shahminanazirah@gmail.com>
2026-06-19 15:19:47 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,183 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner(0);
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(window).width() > 992) {
+            if ($(this).scrollTop() > 45) {
+                $('.sticky-top .container').addClass('shadow-sm').css('max-width', '100%');
+            } else {
+                $('.sticky-top .container').removeClass('shadow-sm').css('max-width', $('.topbar .container').width());
+            }
+        } else {
+            $('.sticky-top .container').addClass('shadow-sm').css('max-width', '100%');
+        }
+    });
+
+
+    // Hero Header carousel
+    $(".header-carousel").owlCarousel({
+        items: 1,
+        autoplay: false,
+        smartSpeed: 2000,
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 0,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ]
+    });
+
+
+
+    // Project carousel
+    $(".project-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : false,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:2
+            }
+        }
+    });
+
+    // testimonial carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : false,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:2
+            }
+        }
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 5,
+        time: 2000
+    });
+
+
+    
+   // Back to top button
+   $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+})(jQuery);
+
+$(".header-carousel").owlCarousel({
+    autoplay: false,         // disable auto slide
+    autoplayTimeout: 0,
+    loop: true,
+    nav: true,
+    dots: false,
+    items: 1,
+    navText: [
+        '<i class="bi bi-chevron-left"></i>',
+        '<i class="bi bi-chevron-right"></i>'
+    ]
+});
+
+// ARIIMS Automatic Client Logo Slider
+$(".client-logo-carousel").owlCarousel({
+    autoplay: true,
+    slideTransition: 'linear', // Creates a smooth continuous motion
+    autoplayTimeout: 3000,     // Time before moving to the next set (in ms)
+    smartSpeed: 1000,          // Speed of the sliding transition (in ms)
+    loop: true,
+    dots: false,
+    nav: false,
+    autoplayHoverPause: true,  // Pauses sliding when user hovers mouse over it
+    responsive: {
+        0:{
+            items:2,
+            margin:10
+        },
+        576:{
+            items:3,
+            margin:15
+        },
+        768:{
+            items:3,
+            margin:20
+        },
+        992:{
+            items:4,
+            margin:20
+        }
+    }
+});
